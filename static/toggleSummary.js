86c2d85474a60fd897ef584d07856c0bc4dd496f
Shiven Khare <143078939+shivenK10@users.noreply.github.com>
2024-11-28 09:51:28 +0530

    Delete static/toggleSummary.js

--- static/toggleSummary.js
+++
@@ -1,8 +0,0 @@
-function toggleSummary() {
-    var summary = document.getElementById("summary");
-    if (summary.style.display === "none") {
-        summary.style.display = "block";
-    } else {
-        summary.style.display = "none";
-    }
-}
